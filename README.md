1b4936f3f7c631066cf94e35a09bbd6b28f942b7
Joe Oketch Wokiri <wokirijoe@gmail.com>
2021-05-14 14:29:10 +0300

    Added common file and text options
    Files can be saved and or opened, texts can be edited, copied, cut and paste

--- README.md
+++ README.md
@@ -10,4 +10,5 @@
 
 
 ### Besides the above mentioned possibilities, the editor also:
-- displays column and row position everytime the cusor position changes
+- displays column and row position of the cusor, and updates upon any relocation
+- allows for text cut, copy and paste
--- newfrontier_text_editor.py
+++ newfrontier_text_editor.py
@@ -41,6 +41,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("New Frontier Editor")
 
         self.appendActions()
+        self.textAreaGui()
 
         self.filename = None
 
@@ -49,18 +50,42 @@ class MainWindow(QtWidgets.QMainWindow):
         
         # Various FILE options
         self.new_fileAction = QtWidgets.QAction("&New",self)
-
+        self.new_fileAction.setShortcut("Ctrl+N")
+        self.new_fileAction.setStatusTip("Create a new file")
+        self.new_fileAction.triggered.connect(self.new)
 
         self.open_fileAction = QtWidgets.QAction("&Open...",self)
-
+        self.open_fileAction.setStatusTip("Open an existing file")
+        self.open_fileAction.setShortcut("Ctrl+O")
+        self.open_fileAction.triggered.connect(self.open)
 
         self.save_fileAction = QtWidgets.QAction("&Save",self)
+        self.save_fileAction.setStatusTip("Save file")
+        self.save_fileAction.setShortcut("Ctrl+S")
+        self.save_fileAction.triggered.connect(self.save)
 
+        self.cut_fileAction = QtWidgets.QAction("Cut",self)
+        self.cut_fileAction.setStatusTip("Delete and copy text to clipboard")
+        self.cut_fileAction.setShortcut("Ctrl+X")
+        self.cut_fileAction.triggered.connect(self.textAreaGui)
+
+        self.copy_fileAction = QtWidgets.QAction("Copy",self)
+        self.copy_fileAction.setStatusTip("Copy text to clipboard")
+        self.copy_fileAction.setShortcut("Ctrl+C")
+        self.copy_fileAction.triggered.connect(self.textAreaGui)
+
+        self.paste_fileAction = QtWidgets.QAction("Paste",self)
+        self.paste_fileAction.setStatusTip("Paste text from clipboard")
+        self.paste_fileAction.setShortcut("Ctrl+V")
+        self.paste_fileAction.triggered.connect(self.textAreaGui)
 
         return {
             'new_fileAction':self.new_fileAction,
             'open_fileAction':self.open_fileAction,
             'save_fileAction':self.save_fileAction,
+            'cut_fileAction':self.cut_fileAction,
+            'copy_fileAction':self.copy_fileAction,
+            'paste_fileAction':self.paste_fileAction,
         }
 
 
@@ -73,7 +98,77 @@ class MainWindow(QtWidgets.QMainWindow):
         self.file_Toolbar.addAction(self._createEditorActions()['save_fileAction'])
 
 
+        self.file_Toolbar = self.addToolBar("Edit")
+        self.file_Toolbar.addAction(self._createEditorActions()['cut_fileAction'])
+        self.file_Toolbar.addAction(self._createEditorActions()['copy_fileAction'])
+        self.file_Toolbar.addAction(self._createEditorActions()['paste_fileAction'])
+
+
     
+    # Respective Labels for the File, Edit, Search options
+    def textAreaGui(self):
+        self.text = QtWidgets.QTextEdit(self)
+
+
+        font = QtGui.QFont('Terminus', 15, QtGui.QFont.Light)
+        self.text.setFont(QtGui.QFont(font))
+
+        # Add 4 spaces everytime the tab button is pressed
+        self.text.setTabStopDistance(QtGui.QFontMetricsF(self.text.font()).horizontalAdvance(' ') * 4)
+
+        # Adjust the position from a default top-left to the GUI center
+        self.setCentralWidget(self.text)
+
+        # Initialize a statusbar for the window
+        self.statusbar = self.statusBar()
+
+        # If the cursor position changes, call the function that displays the line and column number
+        self.text.cursorPositionChanged.connect(self.cursorPosition)
+
+        return self.text
+
+
+    def new(self):
+        spawn = MainWindow(self)
+        spawn.show()
+
+
+
+    # Function for displaying cursor position
+    def cursorPosition(self):
+        cursor = self.text.textCursor()
+        # Mortals like 1-indexed things
+        line = cursor.blockNumber() + 1
+        col = cursor.columnNumber()
+
+        self.statusbar.showMessage("Line: {} | Column: {}".format(line,col))
+
+
+
+    # Function for opening new file
+    def open(self):
+        filter_options = "python (*.py);; text (*.txt);; php (*.php)"
+        self.filename = QtWidgets.QFileDialog.getOpenFileName(self, caption="Select file", filter=filter_options)[0]
+        
+        
+        if self.filename:
+            # if filename is a valid path to file, open for reading text
+            with open(self.filename,"rt") as file:
+                self.text.setText(file.read())
+
+    def save(self):
+        # Only open dialog if there is no filename yet
+        if not self.filename:
+          self.filename = QtWidgets.QFileDialog.getSaveFileName(self, 'Save File')[0]
+
+        if self.filename:
+            # Save the contents of the text file
+            with open(self.filename,"wt") as file:
+                file.write(self.text.toHtml())
+
+            self.changesSaved = True
+
+
 
 
 
@@ -99,4 +194,3 @@ def main():
 # Call main function if file is module is main
 if __name__ == "__main__":
     main()
-
