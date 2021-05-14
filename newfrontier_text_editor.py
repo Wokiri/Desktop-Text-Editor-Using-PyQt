4a11e59a17b5ff0e562de4024c7c5e763ed89b9f
Joe Oketch Wokiri <wokirijoe@gmail.com>
2021-05-14 07:47:34 +0300

    Initialized Repository
    Created newfrontier_text_editor.py file within which the code for a simple text editor is to be written.

---
+++ newfrontier_text_editor.py
@@ -0,0 +1,17 @@
+'''
+File Name: newfrontier_text_editor.py
+Author: wokirijoe@gmail.com
+Date: 13/05/2021
+
+This python code creates a simple text editor with the ability to:
+    1. Open existing file for editing
+    2. Create a new file
+    3. Save an existing edited file
+    4. Save As functionality
+    5. Open the following file types: ".txt", ".py",".php"
+
+Libraries used:
+    1. PyQt5 version 5.15.4
+    2. python version 3.8.6
+'''
+
